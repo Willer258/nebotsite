a1a380e50074d69f644a8ef8be0ec840972d2ea7
WillAssur <wilfried@wiassur.com>
2023-12-17 19:22:36 +0000

    feat: ajout d'un header potable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import LinkComponent from './LinkComponent'
 function Header() {
   return (
     <div className='fixed top-0 left-0 right-0 sadow-lg'>
  <div className='  container mx-auto   py-8  rounded- bg-white flex justify-between items-center'>
         <Image src={'/images/logo.png'} alt='nebot-patrimoine' height={20} width={200} />
 
-        <div className='flex items-center w-1/2 justify-between font-semibold'>
-                <Link href={'#'}>
-                    Accueil
-                </Link>
-
-                <Link href={'#'}>
-                    Financer
-                </Link>
-                <Link href={'#'}>
-                    Formation
-                </Link>
-                <Link href={'#'}>
-                    Ajouter catalogue
-                </Link>
+        <div className='flex items-center min-w-1/3 justify-between font-semibold'>
+               <LinkComponent name="Formation"/>
+               <LinkComponent name="Catalogue"/>
+               <LinkComponent url='https://www.helloasso.com/associations/institut-nebot-patrimoine/adhesions/adhesion-institut-nebotpatrimoine' name="Devenir membre"/>
+               <LinkComponent url='https://www.helloasso.com/associations/institut-nebot-patrimoine/formulaires/1' name="Faire un don"/>
         </div>
     </div>
 
--- src/components/LinkComponent.tsx
+++ src/components/LinkComponent.tsx
@@ -1,8 +1,39 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import  {useState} from "react";
 
-function LinkComponent({link, name}:any) {
-  return <Link href={link ?? '#'}>{name}</Link>;
+function LinkComponent({ link, name , url}: any) {
+
+  const [hover, setHover] = useState(false)
+
+
+
+  return (
+    <div onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className="relative px-5 py-3 rounded  overflow-hidden  boder">
+      <div  style={hover?{right:'0%'}:{right:'100%'}} className="bg-yellow-300 absolute top-0 duration-300  h-1 w-1/2 "></div>
+      <div
+       style={hover?{left:'0%'}:{left:'100%'}}
+        className="bg-blue-300 absolute duration-300  bottom-0  h-1  w-1/2 "
+      ></div>
+      {
+        link && <Link
+        className="absolute top-0 right-0 left-0 bottom-0"
+        style={{ zIndex: 500 }}
+        href={link ?? "#"}
+      />
+      }
+
+      {
+        url && <a href={url} target="blank"   style={{ zIndex: 500 }}   className="absolute top-0 right-0 left-0 bottom-0"/>
+      }
+      
+
+      <span style={hover?{color:'black'} : {}} className="relative duration-300 text-gray-600 z-50 top-0 left-0 right-0 bottom-0">
+       
+        {name ?? "Link"}
+      </span>
+    </div>
+  );
 }
 
 export default LinkComponent;
